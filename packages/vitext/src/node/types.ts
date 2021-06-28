ad4b565329a0fda0f18c8f7af6c2a42a4ee8b27f
aslemammad <zorofight94@gmail.com>
2021-06-28 19:10:27 +0430

    feat: new types

--- packages/vitext/src/node/types.ts
+++ packages/vitext/src/node/types.ts
@@ -1,6 +1,51 @@
+/// <reference types="node" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+import { IncomingMessage, ServerResponse } from 'http';
+import { ParsedUrlQuery } from 'querystring';
+import React from 'react';
+
+import { getEntries } from './route/pages';
+
+export type Entries = ReturnType<typeof getEntries>;
+
+export type GetPathsContext = {};
+
+export type GetPathsResult<P extends ParsedUrlQuery = ParsedUrlQuery> = {
+  paths: Array<string | { params: P }>;
+};
+
+export type GetPaths<P extends ParsedUrlQuery = ParsedUrlQuery> = (
+  context: GetPathsContext
+) => Promise<GetPathsResult<P>> | GetPathsResult<P>;
+
+export type GetPropsContext<Q extends ParsedUrlQuery = ParsedUrlQuery> = {
+  req: IncomingMessage;
+  res: ServerResponse;
+  params?: Q;
+  query: ParsedUrlQuery;
+};
+
+type JustProps<P> = { props: P; revalidate?: number | boolean };
+type NotFound = { notFound?: true };
+
+export type GetPropsResult<P> = JustProps<P> & NotFound
+
+export type GetProps<
+  P extends { [key: string]: any } = { [key: string]: any },
+  Q extends ParsedUrlQuery = ParsedUrlQuery
+> = (context: GetPropsContext<Q>) => Promise<GetPropsResult<P>>;
+
+export type InferGetPropsType<T> = T extends GetProps<infer P, any>
+  ? P
+  : T extends (
+      context?: GetPropsContext<any>
+    ) => Promise<GetPropsResult<infer P>>
+  ? P
+  : never;
+
 export interface PageFileType {
   default: React.ComponentType<any>;
-  getServerSideProps: any;
-  getStaticProps: any;
-  getStaticPaths: any;
+  getProps: GetProps;
+  getPaths: GetPaths;
 }
