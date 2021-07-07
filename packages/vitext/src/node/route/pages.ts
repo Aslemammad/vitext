8cad732d59f347fa7f70d8f6e7272edf11cb1042
aslemammad <zorofight94@gmail.com>
2021-07-07 17:44:20 +0430

    feat: stable version

--- packages/vitext/src/node/route/pages.ts
+++ packages/vitext/src/node/route/pages.ts
@@ -4,9 +4,20 @@ function isSpecialPage(pagePath: string) {
   return /\/_(document|app)$/.test(pagePath);
 }
 
+function isSameRoute(a: string, b: string) {
+  return b.endsWith('/')
+    ? a.endsWith('/')
+      ? a === b
+      : a + '/' === b
+    : a.endsWith('/')
+    ? a === b + '/'
+    : a === b;
+}
 export type PageType = ReturnType<typeof resolvePagePath> & {};
 
 export function resolvePagePath(pagePath: string, keys: string[]) {
+
+
   const pagesMap = keys.map((page) => {
     let test = page;
     let parts = [];
@@ -16,9 +27,11 @@ export function resolvePagePath(pagePath: string, keys: string[]) {
     if (isDynamic) {
       for (const match of page.matchAll(/\[(\w+)\]/g)) {
         parts.push(match[1]);
+        console.log(page.matchAll(/\[(\w+)\]/g))
       }
 
       test = test.replace(DYNAMIC_PAGE, () => '([^/]+)');
+      console.log(test)
     }
 
     test = test
@@ -35,10 +48,12 @@ export function resolvePagePath(pagePath: string, keys: string[]) {
     };
   });
 
+  console.log(pagePath,keys,pagesMap)
   /**
    * First, try to find an exact match.
    */
-  let page = pagesMap.find((p) => pagePath === p.pagePath);
+  // let page = pagesMap.find((p) => pagePath === p.pagePath );
+  let page = pagesMap.find((p) => pagePath === p.pagePath || isSameRoute(pagePath, p.pagePath));
 
   /**
    * If there's no exact match and the user is requesting a special page,
@@ -106,4 +121,153 @@ export function getEntries(pageManifest: string[]) {
     });
   });
   return entries;
+}
+
+
+
+export interface Group {
+  pos: number;
+  repeat: boolean;
+  optional: boolean;
+}
+
+// this isn't importing the escape-string-regex module
+// to reduce bytes
+function escapeRegex(str: string) {
+  return str.replace(/[|\\{}()[\]^$+*?.-]/g, "\\$&");
+}
+
+function parseParameter(param: string) {
+  const optional = param.startsWith("[") && param.endsWith("]");
+  if (optional) {
+    param = param.slice(1, -1);
+  }
+  const repeat = param.startsWith("...");
+  if (repeat) {
+    param = param.slice(3);
+  }
+  return { key: param, repeat, optional };
+}
+
+// from next.js
+export function getRouteRegex(
+  normalizedRoute: string
+): {
+  re: RegExp;
+  namedRegex?: string;
+  routeKeys?: { [named: string]: string };
+  groups: { [groupName: string]: Group };
+} {
+  const segments = (normalizedRoute.replace(/\/$/, "") || "/")
+    .slice(1)
+    .split("/");
+  const groups: { [groupName: string]: Group } = {};
+  let groupIndex = 1;
+  const parameterizedRoute = segments
+    .map((segment) => {
+      if (segment.startsWith("[") && segment.endsWith("]")) {
+        const { key, optional, repeat } = parseParameter(segment.slice(1, -1));
+        groups[key] = { pos: groupIndex++, repeat, optional };
+        return repeat ? (optional ? "(?:/(.+?))?" : "/(.+?)") : "/([^/]+?)";
+      } else {
+        return `/${escapeRegex(segment)}`;
+      }
+    })
+    .join("");
+
+    let routeKeyCharCode = 97;
+    let routeKeyCharLength = 1;
+
+    // builds a minimal routeKey using only a-z and minimal number of characters
+    const getSafeRouteKey = () => {
+      let routeKey = "";
+
+      for (let i = 0; i < routeKeyCharLength; i++) {
+        routeKey += String.fromCharCode(routeKeyCharCode);
+        routeKeyCharCode++;
+
+        if (routeKeyCharCode > 122) {
+          routeKeyCharLength++;
+          routeKeyCharCode = 97;
+        }
+      }
+      return routeKey;
+    };
+
+    const routeKeys: { [named: string]: string } = {};
+
+    let namedParameterizedRoute = segments
+      .map((segment) => {
+        if (segment.startsWith("[") && segment.endsWith("]")) {
+          const { key, optional, repeat } = parseParameter(
+            segment.slice(1, -1)
+          );
+          // replace any non-word characters since they can break
+          // the named regex
+          let cleanedKey = key.replace(/\W/g, "");
+          let invalidKey = false;
+
+          // check if the key is still invalid and fallback to using a known
+          // safe key
+          if (cleanedKey.length === 0 || cleanedKey.length > 30) {
+            invalidKey = true;
+          }
+          if (!isNaN(parseInt(cleanedKey.substr(0, 1)))) {
+            invalidKey = true;
+          }
+
+          if (invalidKey) {
+            cleanedKey = getSafeRouteKey();
+          }
+
+          routeKeys[cleanedKey] = key;
+          return repeat
+            ? optional
+              ? `(?:/(?<${cleanedKey}>.+?))?`
+              : `/(?<${cleanedKey}>.+?)`
+            : `/(?<${cleanedKey}>[^/]+?)`;
+        } else {
+          return `/${escapeRegex(segment)}`;
+        }
+      })
+      .join("");
+
+    return {
+      re: new RegExp(`^${parameterizedRoute}(?:/)?$`),
+      groups,
+      routeKeys,
+      namedRegex: `^${namedParameterizedRoute}(?:/)?$`
+    };
+}
+
+export function getRouteMatcher(routeRegex: ReturnType<typeof getRouteRegex>) {
+  const { re, groups } = routeRegex;
+  return (pathname: string | null | undefined) => {
+    const routeMatch = re.exec(pathname!);
+    if (!routeMatch) {
+      return false;
+    }
+
+    const decode = (param: string) => {
+      try {
+        return decodeURIComponent(param);
+      } catch (_) {
+        throw new Error("failed to decode param");
+      }
+    };
+    const params: { [paramName: string]: string | string[] } = {};
+
+    Object.keys(groups).forEach((slugName: string) => {
+      const g = groups[slugName];
+      const m = routeMatch[g.pos];
+      if (m !== undefined) {
+        params[slugName] = ~m.indexOf("/")
+          ? m.split("/").map((entry) => decode(entry))
+          : g.repeat
+          ? [decode(m)]
+          : decode(m);
+      }
+    });
+    return params;
+  };
 }
