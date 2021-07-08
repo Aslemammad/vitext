470827a0c0a6153ea97deb5f505228001dc0325e
aslemammad <zorofight94@gmail.com>
2021-07-09 03:12:56 +0430

    feat: import component lazily

--- packages/vitext/src/client/main.tsx
+++ packages/vitext/src/client/main.tsx
@@ -2,15 +2,23 @@ import * as ReactDOM from 'react-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
 import App from '/@vitext/_app';
-import Component from '/@vitext/current-page';
 
 const initialData = document.getElementById('__DATA')?.textContent;
-window.__DATA = initialData ? JSON.parse(initialData) : undefined;
+window.__DATA = JSON.parse(initialData!);
+
+const ComponentPromise = import(
+  `./@vitext/hack-import${window.__DATA.pageClientPath}.js`
+);
 
 async function render() {
+
+  const props = window.__DATA.props[window.__DATA.pageClientPath];
+
+  const Component = (await ComponentPromise).default;
+
   ReactDOM.hydrate(
     <HelmetProvider>
-      <App Component={Component} props={{}} />
+      <App Component={Component} props={props} />
     </HelmetProvider>,
     document.getElementById('root')
   );
