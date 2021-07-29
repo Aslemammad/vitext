45b928e8ea843f3aed00b64192ae8f7f508fc447
aslemammad <zorofight94@gmail.com>
2021-07-29 09:59:42 +0430

    feat: change fetching

--- packages/vitext/src/node/route/fetch.ts
+++ packages/vitext/src/node/route/fetch.ts
@@ -1,30 +1,39 @@
 import isEqual from 'deep-equal';
 import type { ServerResponse } from 'http';
 import * as querystring from 'querystring';
-import type { Connect } from 'vite';
+import type { ConfigEnv, Connect } from 'vite';
 
 import type { GetPaths, GetProps, PageFileType } from '../types';
 import type { PageType } from './pages';
 
 export async function fetchData({
+  env,
   req,
   res,
   pageFile,
   page,
+  isGenerating,
 }: {
-  req: Connect.IncomingMessage;
-  res: ServerResponse;
+  env: ConfigEnv;
+  req?: Connect.IncomingMessage;
+  res?: ServerResponse;
   pageFile: PageFileType;
   page: PageType;
-}): Promise<{ props?: any } | undefined> {
-  const query = querystring.parse(req.originalUrl!);
+  isGenerating: boolean;
+}) {
+  const query = querystring.parse(req?.originalUrl!);
 
   let params: querystring.ParsedUrlQuery | undefined;
 
   // non-dynamic pages should not have getPaths
-  if ('getPaths' in pageFile && page.page.includes('[')) {
-    const { paths } = await fetchPaths({ getPaths: pageFile.getPaths });
+  if (
+    env.mode === 'development' &&
+    'getPaths' in pageFile &&
+    page.pageEntry.pageName.includes('[')
+  ) {
+    const { paths } = await fetchPaths({ getPaths: pageFile.getPaths! });
 
+    console.log(paths)
     params = paths.find((p) =>
       isEqual(page.params, p.params, { strict: false })
     )?.params;
@@ -36,10 +45,10 @@ export async function fetchData({
       res,
       query,
       params,
-      getProps: pageFile.getProps,
+      getProps: pageFile.getProps!,
     });
-    if (getPropsResult.notFound) {
-      res.statusCode = 404;
+    if (!isGenerating && getPropsResult.notFound) {
+      res!.statusCode = 404;
       return;
     }
     if (getPropsResult.revalidate) {
@@ -49,22 +58,22 @@ export async function fetchData({
   return;
 }
 
-async function fetchProps({
+export function fetchProps({
   req,
   res,
   query,
   getProps,
   params,
 }: {
-  req: Connect.IncomingMessage;
-  res: ServerResponse;
-  query: querystring.ParsedUrlQuery;
+  req?: Connect.IncomingMessage;
+  res?: ServerResponse;
+  query?: querystring.ParsedUrlQuery;
   getProps: GetProps;
   params?: querystring.ParsedUrlQuery;
 }) {
-  return await getProps({ req, res, query, params });
+  return getProps({ req, res, query: query || {}, params });
 }
 
-async function fetchPaths({ getPaths }: { getPaths: GetPaths }) {
-  return await getPaths();
+export function fetchPaths({ getPaths }: { getPaths: GetPaths }) {
+  return getPaths();
 }
