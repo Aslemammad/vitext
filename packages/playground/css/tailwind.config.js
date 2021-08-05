c35eb7ff93d7c542740ae57c297485e43df8799c
aslemammad <zorofight94@gmail.com>
2021-08-05 17:34:04 +0430

    feat: css tests

--- packages/playground/css/tailwind.config.js
+++ packages/playground/css/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: process.env.NODE_ENV === 'production' ? '' : 'jit',
-  purge: ['./index.html', './pages/**/*.{js,ts,jsx,tsx}', './styles/**/*'],
+  purge: ['./index.html', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
