1e1d51aa56183150113627af686213526c70de72
aslemammad <zorofight94@gmail.com>
2021-08-10 01:23:05 +0430

    refactor: delete some stuff

--- packages/vitext/src/client/PageLoader.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface Props {
-    routePath: string;
-}
-declare const PageLoader: React.MemoExoticComponent<({ routePath }: Props) => JSX.Element>;
-export default PageLoader;
-//# sourceMappingURL=PageLoader.d.ts.map
