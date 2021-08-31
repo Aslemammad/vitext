47263ac0f0f51a23f14be49cea53099a48f74dd6
aslemammad <zorofight94@gmail.com>
2021-08-31 15:28:55 +0430

    fix: handle no Script

--- packages/playground/custom-document-app/pages/_document.tsx
+++ packages/playground/custom-document-app/pages/_document.tsx
@@ -5,7 +5,6 @@ export default class extends BaseDocument {
     return (
       <>
         <Main />
-        <Script />
       </>
     );
   }
--- packages/vitext/src/client/main.tsx
+++ packages/vitext/src/client/main.tsx
@@ -7,28 +7,34 @@ import App from '/@vitext/_app';
 
 const root = document.getElementById('root');
 const initialData = document.getElementById('__DATA')?.textContent;
-window.__DATA = JSON.parse(initialData!);
+window.__DATA = initialData ? JSON.parse(initialData!) : undefined;
 
-const ComponentPromise = import(
-  `./@vitext/hack-import${window.__DATA.pageClientPath}.js`
-);
+(function () {
+  if (!window.__DATA) {
+    return;
+  }
 
-async function render() {
-  const props = window.__DATA.props[window.__DATA.pageClientPath];
-
-  const Component = (await ComponentPromise).default;
-
-  const element = (
-    <HelmetProvider>
-      <App Component={Component} props={props} />
-    </HelmetProvider>
+  const ComponentPromise = import(
+    `./@vitext/hack-import${window.__DATA.pageClientPath}.js`
   );
 
-  if (import.meta.env.DEV) {
-    ReactDOM.render(element, root);
-  } else {
-    ReactDOM.hydrate(element, root);
-  }
-}
+  async function render() {
+    const props = window.__DATA.props[window.__DATA.pageClientPath];
 
-render();
+    const Component = (await ComponentPromise).default;
+
+    const element = (
+      <HelmetProvider>
+        <App Component={Component} props={props} />
+      </HelmetProvider>
+    );
+
+    if (import.meta.env.DEV) {
+      ReactDOM.render(element, root);
+    } else {
+      ReactDOM.hydrate(element, root);
+    }
+  }
+
+  render();
+})();
