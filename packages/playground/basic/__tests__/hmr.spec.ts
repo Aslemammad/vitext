9c74f84bc8d529a8e08e2c2822a06837815ebdc5
aslemammad <zorofight94@gmail.com>
2021-08-31 15:23:14 +0430

    fix: add (skip) tests

--- packages/playground/basic/__tests__/hmr.spec.ts
+++ packages/playground/basic/__tests__/hmr.spec.ts
@@ -1,28 +1,25 @@
 import { untilUpdated, editFile, isBuild } from '../../testUtils';
 
-if (!isBuild) {
-  test('Editing a page', async () => {
-    await untilUpdated(() => page.textContent('#test'), 'IndexPage');
-    editFile('pages/index.tsx', (code) =>
-      code.replace(
-        '<div id="test">IndexPage</div>',
-        '<div id="test">IndexPage hmr</div>'
-      )
-    );
-    await untilUpdated(
-      () => page.textContent('#hmr-test-page'),
-      'IndexPage hmr'
-    );
-  });
+globalThis.test = !isBuild ? test : test.skip;
 
-  test('Editing a component', async () => {
-    await untilUpdated(() => page.textContent('#test'), 'IndexPage');
-    editFile('components/Component.tsx', (code) =>
-      code.replace('<>loaded</>', '<>loaded hmr</>')
-    );
-    await untilUpdated(
-      () => page.textContent('#hmr-test-component'),
-      'loaded hmr'
-    );
-  });
-}
+test('Editing a page', async () => {
+  await untilUpdated(() => page.textContent('#test'), 'IndexPage');
+  editFile('pages/index.tsx', (code) =>
+    code.replace(
+      '<div id="test">IndexPage</div>',
+      '<div id="test">IndexPage hmr</div>'
+    )
+  );
+  await untilUpdated(() => page.textContent('#hmr-test-page'), 'IndexPage hmr');
+});
+
+test('Editing a component', async () => {
+  await untilUpdated(() => page.textContent('#test'), 'IndexPage');
+  editFile('components/Component.tsx', (code) =>
+    code.replace('<>loaded</>', '<>loaded hmr</>')
+  );
+  await untilUpdated(
+    () => page.textContent('#hmr-test-component'),
+    'loaded hmr'
+  );
+});
